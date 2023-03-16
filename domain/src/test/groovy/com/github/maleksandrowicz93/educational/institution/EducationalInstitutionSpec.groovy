756f96b630619775b0fb538304c60ec0aa5c7b59
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-03-16 10:35:48 +0100

    correct domain spec

--- domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/EducationalInstitutionSpec.groovy
+++ domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/EducationalInstitutionSpec.groovy
@@ -7,7 +7,8 @@ class EducationalInstitutionSpec extends Specification {
     def "faculty may be created"() {
         given: "Educational Institution without any faculty"
         and: "new faculty to be created"
-        expect: "the faculty should be created"
+        when: "the faculty is created"
+        then: "should be created successfully"
     }
 
     def "professor matching all requirements may be employed"() {
@@ -35,7 +36,8 @@ class EducationalInstitutionSpec extends Specification {
         given: "Educational Institution with a professor hired at a faculty"
         when: "professor resigns from employment"
         then: "all courses led by him become vacated"
-        and: "professor is removed from faculty"
+        and: "professor is marked as inactive"
+        and: "external system should be notified"
     }
 
     def "student matching all requirements may be enrolled"() {
@@ -52,7 +54,7 @@ class EducationalInstitutionSpec extends Specification {
         then: "student should not be enrolled"
     }
 
-    def "student should not be employed when no vacancy at faculty"() {
+    def "student should not be enrolled when no vacancy at faculty"() {
         given: "Educational Institution with no vacancy at a faculty"
         and: "student's application matching all requirements"
         when: "student applies for enrollment"
@@ -63,7 +65,8 @@ class EducationalInstitutionSpec extends Specification {
         given: "Educational Institution with a student enrolled at a faculty"
         when: "student resigns from enrollment"
         then: "all courses student is enrolled for become vacated"
-        and: "student is removed from faculty"
+        and: "student is marked as inactive"
+        and: "external system should be notified"
     }
 
     def "professor with capacity may create a course matching all requirements within a faculty"() {
@@ -91,29 +94,50 @@ class EducationalInstitutionSpec extends Specification {
         given: "Educational Institution with a professor leading the course at a faculty"
         when: "professor resigns from leading the course"
         then: "the course becomes vacated"
+        and: "external system should be notified"
     }
 
     def "vacated course may be overtaken by a professor matching all requirements"() {
         given: "Educational Institution with the vacated course within a faculty"
-        when: "professor matching all requirements overtakes the course"
+        and: "employed professor matching all requirements"
+        when: "this professor overtakes the course"
         then: "course should be overtaken"
     }
 
     def "vacated course should not be overtaken by a professor not matching requirements"() {
         given: "Educational Institution with the vacated course within a faculty"
-        when: "professor not matching requirements overtakes the course"
+        and: "employed professor not matching requirements"
+        when: "this professor overtakes the course"
+        then: "course should not be overtaken"
+    }
+
+    def "vacated course should not be overtaken by a professor from other faculty"() {
+        given: "Educational Institution with 2 faculties"
+        and: "vacated course within first faculty"
+        and: "professor matching all requirements employed in second faculty"
+        when: "professor overtakes the curse"
         then: "course should not be overtaken"
     }
 
     def "student may enroll for the vacated course"() {
         given: "Educational Institution with the vacated course within a faculty"
-        when: "student enrolls for a course"
+        and: "student enrolled in the faculty"
+        when: "student enrolls for the course"
         then: "student should be enrolled"
     }
 
     def "student should not be enrolled for the full course"() {
         given: "Educational Institution with the full course within a faculty"
-        when: "student enrolls for a course"
+        and: "student enrolled in the faculty"
+        when: "student enrolls for the course"
+        then: "student should not be enrolled"
+    }
+
+    def "student should not be enrolled for the vacated course from other faculty"() {
+        given: "Educational Institution with 2 faculties"
+        and: "vacated course within first faculty"
+        and: "student enrolled in second faculty"
+        when: "student enrolls for the course"
         then: "student should not be enrolled"
     }
 
@@ -122,9 +146,10 @@ class EducationalInstitutionSpec extends Specification {
         and: "too many vacancies within the course"
         when: "professor closes the course"
         then: "the course should be closed"
+        and: "external system should be notified"
     }
 
-    def "course with enough number of enrolled students fo should not be closed"() {
+    def "course with enough number of enrolled students should not be closed"() {
         given: "Educational Institution with the course within a faculty"
         and: "enough number of students are enrolled for the course"
         when: "professor closes the course"
