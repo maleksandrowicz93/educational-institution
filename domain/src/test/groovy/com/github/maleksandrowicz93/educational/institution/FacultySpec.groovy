267a90f3f16086cea78931fe28e0f972e0ef7ca7
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-04 09:42:54 +0200

    create FacultySpec

---
+++ domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/FacultySpec.groovy
@@ -0,0 +1,380 @@
+package com.github.maleksandrowicz93.educational.institution
+
+import com.github.maleksandrowicz93.educational.institution.enums.CourseState
+import com.github.maleksandrowicz93.educational.institution.enums.EmploymentState
+import com.github.maleksandrowicz93.educational.institution.enums.EnrollmentState
+import com.github.maleksandrowicz93.educational.institution.results.CourseCreationResultReason
+import com.github.maleksandrowicz93.educational.institution.results.EnrollmentResultReason
+import com.github.maleksandrowicz93.educational.institution.results.HiringResultReason
+import com.github.maleksandrowicz93.educational.institution.vo.CourseId
+import com.github.maleksandrowicz93.educational.institution.vo.FacultyId
+import com.github.maleksandrowicz93.educational.institution.vo.Threshold
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static FacultyUtils.basicCourseProposition
+import static FacultyUtils.basicProfessorApplication
+import static FacultyUtils.basicStudentApplication
+import static FacultyUtils.courseProposition
+import static FacultyUtils.fakePersonalData
+import static FacultyUtils.professorApplication
+import static FacultyUtils.studentApplication
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.MAIN_FIELD_OF_STUDY
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.SECONDARY_FIELDS_OF_STUDY
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.fieldOfStudy
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.newFaculty
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.professor
+import static com.github.maleksandrowicz93.educational.institution.FacultyUtils.student
+import static java.util.stream.Collectors.toSet
+
+class FacultySpec extends Specification {
+
+    def "professor matching all requirements may be employed"() {
+        given: "faculty with a vacancy"
+        def snapshot = newFaculty()
+        def faculty = Faculty.from(snapshot)
+
+        and: "professor's application matching all requirements"
+        def application = basicProfessorApplication(snapshot.id())
+
+        when: "professor applies for hiring"
+        def hiringResult = faculty.considerHiring(application)
+
+        then: "professor should be employed"
+        def professors = faculty.createSnapshot().professors()
+        def maybeHired = hiringResult.value()
+        maybeHired.isPresent()
+        with(maybeHired.get()) {
+            professors.contains(it)
+            id().value() != null
+            personalData() == application.personalData()
+            facultyId() == application.facultyId()
+            fieldsOfStudy() == application.fieldsOfStudy()
+            ledCourses().isEmpty()
+            employmentState() == EmploymentState.EMPLOYED
+        }
+    }
+
+    @Unroll("years of experience: #yearsOfExperience, fields of study: #fieldsOfStudyNames, failure reason: #resultReason.text()")
+    def "professor not matching requirements should not be employed"() {
+        given: "faculty with a vacancy"
+        def snapshot = newFaculty()
+        def faculty = Faculty.from(snapshot)
+
+        and: "professor's application not matching all requirements"
+        def fieldsOfStudy = buildFieldsOfStudyFrom(fieldsOfStudyNames, snapshot.id())
+        def application = professorApplication(snapshot.id(), yearsOfExperience, fieldsOfStudy)
+
+        when: "professor applies for hiring"
+        def hiringResult = faculty.considerHiring(application)
+
+        then: "professor should not be employed"
+        def professors = faculty.createSnapshot().professors()
+        professors.isEmpty()
+        hiringResult.value().isEmpty()
+        hiringResult.resultReason() == resultReason
+
+        where:
+        yearsOfExperience | fieldsOfStudyNames          | resultReason
+        1                 | SECONDARY_FIELDS_OF_STUDY   | HiringResultReason.TOO_LITTLE_EXPERIENCE
+        2                 | Set.of(MAIN_FIELD_OF_STUDY) | HiringResultReason.FIELDS_OF_STUDY_NOT_MATCHED
+        1                 | Set.of(MAIN_FIELD_OF_STUDY) | HiringResultReason.TOO_LITTLE_EXPERIENCE
+    }
+
+    private def buildFieldsOfStudyFrom(Set<String> fieldsOfStudyNames, FacultyId facultyId) {
+        fieldsOfStudyNames.stream()
+                .map { fieldOfStudy(it, facultyId) }
+                .collect(toSet())
+    }
+
+    def "professor should not be employed when no vacancy at faculty"() {
+        given: "faculty with no vacancy"
+        def newFaculty = newFaculty()
+        def snapshot = newFaculty.toBuilder()
+                .professor(professor(newFaculty.id()))
+                .professor(professor(newFaculty.id()))
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "professor's application matching all requirements"
+        def application = basicProfessorApplication(snapshot.id())
+
+        when: "professor applies for hiring"
+        def hiringResult = faculty.considerHiring(application)
+
+        then: "professor should not be employed"
+        def professors = faculty.createSnapshot().professors()
+        professors.isEmpty()
+        hiringResult.value().isEmpty()
+        hiringResult.resultReason() == HiringResultReason.NO_VACANCY
+    }
+
+    def "professor may resign from employment"() {
+        given: "faculty with hired professor leading 2 courses"
+        def newFaculty = newFaculty()
+        def courses = Set.of(new CourseId(UUID.randomUUID()), new CourseId(UUID.randomUUID()))
+        def hired = professor(newFaculty.id()).toBuilder()
+                .ledCourses(courses)
+                .build()
+        def snapshot = newFaculty.toBuilder()
+                .professor(hired)
+                .courses(courses)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        when: "professor resigns from employment"
+        def resigned = faculty.receiveHiringResignation(hired.id())
+
+        then: "all courses led by him become vacated"
+        def currentSnapshot = faculty.createSnapshot()
+        def ledCourses = currentSnapshot.professors().stream()
+                .map { it.ledCourses() }
+                .flatMap { it.stream() }
+                .collect(toSet())
+        ledCourses.isEmpty()
+
+        and: "professor is marked as inactive"
+        resigned.employmentState() == EmploymentState.INACTIVE
+
+        and: "external system should be notified"
+        1 * eventsPublisher.publish(_)
+    }
+
+    def "student matching all requirements may be enrolled"() {
+        given: "faculty with a vacancy"
+        def snapshot = newFaculty()
+        def faculty = Faculty.from(snapshot)
+
+        and: "student's application matching all requirements"
+        def application = basicStudentApplication(snapshot)
+
+        when: "student applies for enrollment"
+        def enrollmentResult = faculty.considerEnrollment(application)
+
+        then: "student should be enrolled"
+        def students = faculty.createSnapshot().students()
+        def maybeEnrolled = enrollmentResult.value()
+        maybeEnrolled.isPresent()
+        with(maybeEnrolled.get()) {
+            students.contains(it)
+            id().value() != null
+            fakePersonalData() == application.personalData()
+            facultyId() == snapshot.id()
+            enrollmentState() == EnrollmentState.ENROLLED
+        }
+    }
+
+    @Unroll("main test result: #mainTestResult, secondary tests result: #secondaryTestResult, failure reason: #resultReason.text()")
+    def "student not matching requirements should not be enrolled"() {
+        given: "faculty with a vacancy"
+        def snapshot = newFaculty()
+        def faculty = Faculty.from(snapshot)
+
+        and: "student's application not matching requirements"
+        def application = studentApplication(snapshot, mainTestResult, secondaryTestResult)
+
+        when: "student applies for enrollment"
+        def enrollmentResult = faculty.considerEnrollment(application)
+
+        then: "student should not be enrolled"
+        def students = faculty.createSnapshot().students();
+        students.isEmpty()
+        enrollmentResult.value().isEmpty()
+        enrollmentResult.resultReason() == resultReason
+
+        where:
+        mainTestResult | secondaryTestResult | resultReason
+        80             | 0                   | EnrollmentResultReason.SECONDARY_FIELDS_OF_STUDY_TESTS_NOT_PASSED
+        0              | 80                  | EnrollmentResultReason.MAIN_FIELD_OF_STUDY_TEST_NOT_PASSED
+        0              | 0                   | EnrollmentResultReason.MAIN_FIELD_OF_STUDY_TEST_NOT_PASSED
+    }
+
+    def "student should not be enrolled when no vacancy at faculty"() {
+        given: "faculty with no vacancy"
+        def newFaculty = newFaculty()
+        def snapshot = newFaculty.toBuilder()
+                .student(student(newFaculty.id()))
+                .student(student(newFaculty.id()))
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "student's application matching all requirements"
+        def application = basicStudentApplication(snapshot)
+
+        when: "student applies for enrollment"
+        def enrollmentResult = faculty.considerEnrollment(application)
+
+        then: "student should not be enrolled"
+        def students = faculty.createSnapshot().students();
+        students.isEmpty()
+        enrollmentResult.value().isEmpty()
+        enrollmentResult.resultReason() == EnrollmentResultReason.NO_VACANCY
+    }
+
+    def "student may resign from enrollment"() {
+        given: "faculty with a student enrolled for 2 courses"
+        def newFaculty = newFaculty()
+        def courses = Set.of(new CourseId(UUID.randomUUID()), new CourseId(UUID.randomUUID()))
+        def student = student(newFaculty.id()).toBuilder()
+                .courses(courses)
+                .build()
+        def snapshot = newFaculty.toBuilder()
+                .student(student)
+                .courses(courses)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        when: "student resigns from enrollment"
+        def resigned = faculty.receiveEnrollmentResignation(student.id())
+
+        then: "all courses student is enrolled for become vacated"
+        def currentSnapshot = faculty.createSnapshot()
+        def coursesStudentIsEnrolledFor = currentSnapshot.students().stream()
+                .map { it.courses() }
+                .flatMap { it.stream() }
+                .collect()
+        coursesStudentIsEnrolledFor.isEmpty()
+
+        and: "student is marked as inactive"
+        resigned.enrollmentState() == EnrollmentState.INACTIVE
+
+        and: "external system should be notified"
+        1 * eventsPublisher.publish(_)
+    }
+
+    def "professor with capacity may create a course matching all requirements within a faculty"() {
+        given: "faculty with hired professor with capacity"
+        def newFaculty = newFaculty()
+        def hired = professor(newFaculty.id())
+        def snapshot = newFaculty.toBuilder()
+                .professor(hired)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "course matching all requirements to be created"
+        def courseProposition = basicCourseProposition(hired, snapshot.id())
+
+        when: "professor creates course"
+        def courseCreationResult = faculty.considerCourseCreation(courseProposition)
+
+        then: "course should be created"
+        def courses = faculty.createSnapshot().courses()
+        def maybeCreated = courseCreationResult.value()
+        maybeCreated.isPresent()
+        with(maybeCreated.get()) {
+            id().value() != null
+            courses.contains(id())
+            name() == courseProposition.name()
+            facultyId() == courseProposition.facultyId()
+            professor().id() == courseProposition.professorId()
+            fieldsOfStudy() == courseProposition.fieldsOfStudy()
+            students().isEmpty()
+            state() == CourseState.LED
+        }
+    }
+
+    @Unroll("fields of study: #fieldsOfStudyNames, failure reason: #resultReason")
+    def "professor with capacity should not create a course not matching requirements within a faculty"() {
+        given: "faculty with hired professor with capacity"
+        def newFaculty = newFaculty()
+        def hired = professor(newFaculty.id())
+        def snapshot = newFaculty.toBuilder()
+                .professor(hired)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "course not matching requirements to be created"
+        def fieldsOfStudy = buildFieldsOfStudyFrom(fieldsOfStudyNames, snapshot.id())
+        def courseProposition = courseProposition(hired, fieldsOfStudy)
+
+        when: "professor creates course"
+        def courseCreationResult = faculty.considerCourseCreation(courseProposition)
+
+        then: "course should not be created"
+        def courses = faculty.createSnapshot().courses()
+        courses.isEmpty()
+        courseCreationResult.value().isEmpty()
+        courseCreationResult.resultReason() == resultReason
+
+        where:
+        fieldsOfStudyNames        | resultReason
+        tooFewFieldsOfStudy()     | CourseCreationResultReason.TOO_FEW_FIELDS_OF_STUDY
+        tooManyFieldsOfStudy()    | CourseCreationResultReason.TOO_MANY_FIELDS_OF_STUDY
+        notMatchedFieldsOfStudy() | CourseCreationResultReason.PROFESSOR_FIELDS_OF_STUDY_NOT_MATCHED
+    }
+
+    private def tooFewFieldsOfStudy() {
+        Set.of(MAIN_FIELD_OF_STUDY)
+    }
+
+    private def tooManyFieldsOfStudy() {
+        def fieldsOfStudyNames = new HashSet<>(SECONDARY_FIELDS_OF_STUDY)
+        fieldsOfStudyNames.add(MAIN_FIELD_OF_STUDY)
+        fieldsOfStudyNames
+    }
+
+    private def notMatchedFieldsOfStudy() {
+        Set.of(MAIN_FIELD_OF_STUDY, "Management and Production Engineering")
+    }
+
+    def "professor with capacity should not create a course matching all requirements within a full faculty"() {
+        given: "faculty with hired professor leading all faculty courses"
+        def newFaculty = newFaculty()
+        def courses = Set.of(new CourseId(UUID.randomUUID()), new CourseId(UUID.randomUUID()))
+        def busyProfessor = professor(newFaculty.id()).toBuilder()
+                .ledCourses(courses)
+                .build()
+        def snapshotBuilder = newFaculty.toBuilder()
+                .professor(busyProfessor)
+                .courses(courses)
+
+        and: "another hired professor with capacity"
+        def freeProfessor = professor(newFaculty.id())
+        def snapshot = snapshotBuilder
+                .professor(freeProfessor)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "new course to be created"
+        def courseProposition = basicCourseProposition(freeProfessor, snapshot.id())
+
+        when: "free professor creates the course"
+        def courseCreationResult = faculty.considerCourseCreation(courseProposition)
+
+        then: "course should not be created"
+        def currentCourses = faculty.createSnapshot().courses()
+        currentCourses == courses
+        courseCreationResult.value().isEmpty()
+        courseCreationResult.resultReason() == CourseCreationResultReason.NO_CAPACITY_AT_FACULTY
+    }
+
+    def "professor with no capacity should not create a course matching all requirements within a faculty"() {
+        given: "Educational Institution allowing maximum one led course per professor"
+        def maximumLedCourses = new Threshold(1)
+        def newFaculty = newFaculty(maximumLedCourses)
+        def snapshotBuilder = newFaculty.toBuilder()
+
+        and: "professor with no capacity hired at a faculty"
+        def courses = Set.of(new CourseId(UUID.randomUUID()))
+        def hired = professor(newFaculty.id()).toBuilder()
+                .ledCourses(courses)
+                .build()
+        def snapshot = snapshotBuilder
+                .professor(hired)
+                .courses(courses)
+                .build()
+        def faculty = Faculty.from(snapshot)
+
+        and: "course matching all requirements to be created"
+        def newCourseProposition = basicCourseProposition(hired, snapshot.id())
+
+        when: "professor creates course"
+        def courseCreationResult = faculty.considerCourseCreation(newCourseProposition)
+
+        then: "course should not be created"
+        def currentCourses = faculty.createSnapshot().courses()
+        currentCourses == courses
+        courseCreationResult.value().isEmpty()
+        courseCreationResult.resultReason() == CourseCreationResultReason.NO_PROFESSOR_CAPACITY
+    }
+}
--- domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/FacultyUtils.groovy
+++ domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/FacultyUtils.groovy
@@ -1,5 +1,7 @@
 package com.github.maleksandrowicz93.educational.institution
 
+import com.github.maleksandrowicz93.educational.institution.enums.EmploymentState
+import com.github.maleksandrowicz93.educational.institution.enums.EnrollmentState
 import com.github.maleksandrowicz93.educational.institution.vo.BasicPersonName
 import com.github.maleksandrowicz93.educational.institution.vo.CourseCreationThresholds
 import com.github.maleksandrowicz93.educational.institution.vo.CourseProposition
@@ -11,6 +13,7 @@ import com.github.maleksandrowicz93.educational.institution.vo.FacultySetup
 import com.github.maleksandrowicz93.educational.institution.vo.FacultySnapshot
 import com.github.maleksandrowicz93.educational.institution.vo.FieldOfStudyId
 import com.github.maleksandrowicz93.educational.institution.vo.FieldOfStudyName
+import com.github.maleksandrowicz93.educational.institution.vo.FieldOfStudySnapshot
 import com.github.maleksandrowicz93.educational.institution.vo.NationalIdentificationNumber
 import com.github.maleksandrowicz93.educational.institution.vo.PersonName
 import com.github.maleksandrowicz93.educational.institution.vo.PersonalData
@@ -18,9 +21,12 @@ import com.github.maleksandrowicz93.educational.institution.vo.PersonalIdentific
 import com.github.maleksandrowicz93.educational.institution.vo.PhoneNumber
 import com.github.maleksandrowicz93.educational.institution.vo.ProfessorApplication
 import com.github.maleksandrowicz93.educational.institution.vo.ProfessorHiringThresholds
+import com.github.maleksandrowicz93.educational.institution.vo.ProfessorId
 import com.github.maleksandrowicz93.educational.institution.vo.ProfessorSnapshot
 import com.github.maleksandrowicz93.educational.institution.vo.StudentApplication
 import com.github.maleksandrowicz93.educational.institution.vo.StudentEnrollmentThresholds
+import com.github.maleksandrowicz93.educational.institution.vo.StudentId
+import com.github.maleksandrowicz93.educational.institution.vo.StudentSnapshot
 import com.github.maleksandrowicz93.educational.institution.vo.TestResult
 import com.github.maleksandrowicz93.educational.institution.vo.Threshold
 import com.github.maleksandrowicz93.educational.institution.vo.YearsOfExperience
@@ -29,53 +35,115 @@ import static java.util.stream.Collectors.toSet
 
 class FacultyUtils {
 
-    private static def BASIC_THRESHOLD = new Threshold(2)
+    static final def FACULTY_NAME = "Mechanical"
+    static final def MAIN_FIELD_OF_STUDY = "Mechanics and Construction of Machines"
+    static final def SECONDARY_FIELDS_OF_STUDY = Set.of("Mechatronics", "Automation and Robotics")
+    static final def BASIC_THRESHOLD = new Threshold(2)
 
     private FacultyUtils() {}
 
     static def facultySetup(EducationalInstitutionId educationalInstitutionId) {
         FacultySetup.builder()
-                .name("Mechanical")
+                .name(FACULTY_NAME)
                 .educationalInstitutionId(educationalInstitutionId)
-                .mainFieldOfStudyName(new FieldOfStudyName("Mechanics and Construction of Machines"))
-                .secondaryFieldsOfStudyNames(Set.of(
-                        new FieldOfStudyName("Mechatronics"),
-                        new FieldOfStudyName("Automation and Robotics")
-                ))
-                .facultyManagementThresholds(FacultyManagementThresholds.builder()
-                        .professorHiringThresholds(ProfessorHiringThresholds.builder()
-                                .minimumYearsOfExperience(BASIC_THRESHOLD)
-                                .minimumKnownFieldsOfStudy(BASIC_THRESHOLD)
-                                .maximumVacancies(BASIC_THRESHOLD)
-                                .build())
-                        .studentEnrollmentThresholds(StudentEnrollmentThresholds.builder()
-                                .minimumMainFieldOfStudyExamScore(BASIC_THRESHOLD)
-                                .minimumSecondaryFieldsOfStudyExamsScore(BASIC_THRESHOLD)
-                                .maximumVacancies(BASIC_THRESHOLD)
-                                .build())
-                        .courseCreationThresholds(CourseCreationThresholds.builder()
-                                .minimumCourseFieldsOfStudy(BASIC_THRESHOLD)
-                                .maximumCourseFieldsOfStudy(BASIC_THRESHOLD)
-                                .maximumFacultyCourses(BASIC_THRESHOLD)
-                                .maximumProfessorCourses(BASIC_THRESHOLD)
-                                .build())
+                .mainFieldOfStudyName(mainFieldOfStudyName())
+                .secondaryFieldsOfStudyNames(secondaryFieldsOfStudyNames())
+                .facultyManagementThresholds(facultyManagementThresholds(BASIC_THRESHOLD))
+                .build()
+    }
+
+    private static def facultyManagementThresholds(Threshold maximumLedCourses) {
+        FacultyManagementThresholds.builder()
+                .professorHiringThresholds(ProfessorHiringThresholds.builder()
+                        .minimumYearsOfExperience(BASIC_THRESHOLD)
+                        .minimumKnownFieldsOfStudy(BASIC_THRESHOLD)
+                        .maximumVacancies(BASIC_THRESHOLD)
+                        .build())
+                .studentEnrollmentThresholds(StudentEnrollmentThresholds.builder()
+                        .minimumMainFieldOfStudyExamScore(BASIC_THRESHOLD)
+                        .minimumSecondaryFieldsOfStudyExamsScore(BASIC_THRESHOLD)
+                        .maximumVacancies(BASIC_THRESHOLD)
+                        .build())
+                .courseCreationThresholds(CourseCreationThresholds.builder()
+                        .minimumCourseFieldsOfStudy(BASIC_THRESHOLD)
+                        .maximumCourseFieldsOfStudy(BASIC_THRESHOLD)
+                        .maximumFacultyCourses(BASIC_THRESHOLD)
+                        .maximumProfessorCourses(maximumLedCourses)
                         .build())
                 .build()
     }
 
-    static def basicFieldsOfStudy(FacultySnapshot faculty) {
-        faculty.secondaryFieldsOfStudy().stream()
+    private static def mainFieldOfStudyName() {
+        return new FieldOfStudyName(MAIN_FIELD_OF_STUDY);
+    }
+
+    private static def secondaryFieldsOfStudyNames() {
+        SECONDARY_FIELDS_OF_STUDY.stream()
+                .map { new FieldOfStudyName(it) }
+                .collect(toSet())
+    }
+
+    static def newFaculty() {
+        newFaculty(BASIC_THRESHOLD)
+    }
+
+    static def newFaculty(Threshold maximumLedCourses) {
+        def facultyId = new FacultyId(UUID.randomUUID())
+        FacultySnapshot.builder()
+                .id(facultyId)
+                .name(FACULTY_NAME)
+                .educationalInstitutionId(new EducationalInstitutionId(UUID.randomUUID()))
+                .facultyManagementThresholds(facultyManagementThresholds(maximumLedCourses))
+                .mainFieldOfStudy(fieldOfStudy(MAIN_FIELD_OF_STUDY, facultyId))
+                .secondaryFieldsOfStudy(secondaryFieldsOfStudy(facultyId))
+                .professors(Set.of())
+                .students(Set.of())
+                .courses(Set.of())
+                .build()
+    }
+
+    static def fieldOfStudy(String fieldOfStudyName, FacultyId facultyId) {
+        FieldOfStudySnapshot.builder()
+                .id(new FieldOfStudyId(UUID.randomUUID()))
+                .name(fieldOfStudyName)
+                .facultyId(facultyId)
+                .build()
+    }
+
+    private static def secondaryFieldsOfStudy(FacultyId facultyId) {
+        SECONDARY_FIELDS_OF_STUDY.stream()
+                .map { fieldOfStudy(it, facultyId) }
+                .collect(toSet())
+    }
+
+    static def basicFieldsOfStudy(FacultyId facultyId) {
+        secondaryFieldsOfStudy(facultyId).stream()
                 .map { it.id() }
                 .collect(toSet())
     }
 
-    static def basicProfessorApplication(FacultySnapshot faculty) {
-        def yearsOfExperience = 3
-        def fieldsOfStudy = basicFieldsOfStudy(faculty)
-        professorApplication(faculty.id(), yearsOfExperience, fieldsOfStudy)
+    static def professor(FacultyId facultyId) {
+        ProfessorSnapshot.builder()
+                .id(new ProfessorId(UUID.randomUUID()))
+                .personalData(fakePersonalData())
+                .facultyId(facultyId)
+                .fieldsOfStudy(secondaryFieldsOfStudy(facultyId))
+                .ledCourses(Set.of())
+                .employmentState(EmploymentState.EMPLOYED)
+                .build()
     }
 
-    static def professorApplication(FacultyId facultyId, int yearsOfExperience, Set<FieldOfStudyId> fieldsOfStudy) {
+    static def basicProfessorApplication(FacultyId facultyId) {
+        def yearsOfExperience = 3
+        def fieldsOfStudy = secondaryFieldsOfStudy(facultyId)
+        professorApplication(facultyId, yearsOfExperience, fieldsOfStudy)
+    }
+
+    static def professorApplication(
+            FacultyId facultyId,
+            int yearsOfExperience,
+            Set<FieldOfStudySnapshot> fieldsOfStudy
+    ) {
         ProfessorApplication.builder()
                 .facultyId(facultyId)
                 .yearsOfExperience(new YearsOfExperience(yearsOfExperience))
@@ -102,6 +170,15 @@ class FacultyUtils {
                 .build()
     }
 
+    static def student(FacultyId facultyId) {
+        StudentSnapshot.builder()
+                .id(new StudentId(UUID.randomUUID()))
+                .personalData(fakePersonalData())
+                .facultyId(facultyId)
+                .enrollmentState(EnrollmentState.ENROLLED)
+                .build()
+    }
+
     static def basicStudentApplication(FacultySnapshot faculty) {
         studentApplication(faculty, 80, 90)
     }
@@ -117,11 +194,11 @@ class FacultyUtils {
                 .build()
     }
 
-    static def basicCourseProposition(ProfessorSnapshot hired, FacultySnapshot faculty) {
-        courseProposition(hired, basicFieldsOfStudy(faculty))
+    static def basicCourseProposition(ProfessorSnapshot hired, FacultyId facultyId) {
+        courseProposition(hired, secondaryFieldsOfStudy(facultyId))
     }
 
-    static def courseProposition(ProfessorSnapshot hired, Set<FieldOfStudyId> fieldsOfStudy) {
+    static def courseProposition(ProfessorSnapshot hired, Set<FieldOfStudySnapshot> fieldsOfStudy) {
         CourseProposition.builder()
                 .name("Machines Construction Fundamentals")
                 .professorId(hired.id())
