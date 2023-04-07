7cf509373e1d8f4b4f97f7f859913660f0904d06
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-07 10:48:11 +0200

    create BaseAggregate

---
+++ commons/src/main/java/com/github/maleksandrowicz93/educational/institution/common/BaseAggregate.java
@@ -0,0 +1,15 @@
+package com.github.maleksandrowicz93.educational.institution.common;
+
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@SuperBuilder
+@FieldDefaults(makeFinal = true)
+public abstract class BaseAggregate<S extends Snapshot<ID>, ID> implements Aggregate<S, ID> {
+
+    @Getter(PROTECTED)
+    S source;
+}
