5bbf03a839f1476caad1aa13e3eb0b023063fd40
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-07 12:34:09 +0200

    adjust tests to domain services refactor

---
+++ domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/FacultyCreationServiceSpec.groovy
@@ -0,0 +1,67 @@
+package com.github.maleksandrowicz93.educational.institution
+
+import com.github.maleksandrowicz93.educational.institution.api.business.FacultyCreationService
+import com.github.maleksandrowicz93.educational.institution.reposiotry.EducationalInstitutionSimpleRepository
+import com.github.maleksandrowicz93.educational.institution.reposiotry.FacultySimpleRepository
+import com.github.maleksandrowicz93.educational.institution.repository.EducationalInstitutionRepository
+import com.github.maleksandrowicz93.educational.institution.repository.FacultyRepository
+import com.github.maleksandrowicz93.educational.institution.results.FacultyCreationResultReason
+import com.github.maleksandrowicz93.educational.institution.vo.EducationalInstitutionId
+import spock.lang.Specification
+
+import static com.github.maleksandrowicz93.educational.institution.utils.EducationalInstitutionUtils.newEducationalInstitution
+import static com.github.maleksandrowicz93.educational.institution.utils.FacultyUtils.facultySetup
+import static com.github.maleksandrowicz93.educational.institution.utils.FacultyUtils.facultySetup
+
+class FacultyCreationServiceSpec extends Specification {
+
+    EducationalInstitutionRepository educationalInstitutionRepository
+    FacultyRepository facultyRepository
+    FacultyCreationService service
+
+    def setup() {
+        educationalInstitutionRepository = new EducationalInstitutionSimpleRepository()
+        facultyRepository = new FacultySimpleRepository()
+        service = new DomainFacultyCreationService(educationalInstitutionRepository, facultyRepository)
+    }
+
+    def "created faculty should be stored in proper repositories"() {
+        given: "educational institution"
+        def educationalInstitution = newEducationalInstitution()
+        educationalInstitutionRepository.save(educationalInstitution)
+
+        and: "faculty setup to be created"
+        def facultySetup = facultySetup(educationalInstitution.id())
+
+        when: "faculty is created"
+        def facultyCreationResult = service.createFaculty(facultySetup)
+
+        then: "should be created successfully"
+        def maybeCreated = facultyCreationResult.value()
+        maybeCreated.isPresent()
+        def created = maybeCreated.get()
+
+        and: "should be stored both in educational institution and faulty repositories"
+        def maybeFaculties = educationalInstitutionRepository.findById(educationalInstitution.id())
+                .map { it.faculties() }
+        maybeFaculties.isPresent()
+        maybeFaculties.get().contains(created.id())
+        facultyRepository.existsById(created.id())
+    }
+
+    def "nothing should be stored in repository at faculty creation failure"() {
+        given: "faculty setup to be created at not existing educational institution"
+        def notKnownId = new EducationalInstitutionId(UUID.randomUUID())
+        def facultySetup = facultySetup(notKnownId)
+
+        when: "faculty is created"
+        def facultyCreationResult = service.createFaculty(facultySetup)
+
+        then: "should not be created successfully"
+        facultyCreationResult.value().isEmpty()
+        facultyCreationResult.resultReason() == FacultyCreationResultReason.INCORRECT_EDUCATIONAL_INSTITUTION_ID
+
+        and: "should not be stored in any repository"
+        !educationalInstitutionRepository.existsById(notKnownId)
+    }
+}
---
+++ domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/FacultyManagementServiceSpec.groovy
@@ -0,0 +1,126 @@
+package com.github.maleksandrowicz93.educational.institution
+
+import com.github.maleksandrowicz93.educational.institution.api.business.FacultyManagementService
+import com.github.maleksandrowicz93.educational.institution.common.EventsPublisher
+import com.github.maleksandrowicz93.educational.institution.common.SimpleEventsPublisher
+import com.github.maleksandrowicz93.educational.institution.events.ProfessorHiringResignationEvent
+import com.github.maleksandrowicz93.educational.institution.events.StudentEnrollmentResignationEvent
+import com.github.maleksandrowicz93.educational.institution.reposiotry.FacultySimpleRepository
+import com.github.maleksandrowicz93.educational.institution.repository.FacultyRepository
+import com.github.maleksandrowicz93.educational.institution.results.EmploymentResignationResultReason
+import com.github.maleksandrowicz93.educational.institution.results.EnrollmentResignationResultReason
+import com.github.maleksandrowicz93.educational.institution.vo.FacultyId
+import spock.lang.Specification
+
+import static com.github.maleksandrowicz93.educational.institution.utils.CandidatesUtils.newProfessor
+import static com.github.maleksandrowicz93.educational.institution.utils.CandidatesUtils.newStudent
+import static com.github.maleksandrowicz93.educational.institution.utils.FacultyUtils.newFaculty
+
+class FacultyManagementServiceSpec extends Specification {
+
+    FacultyRepository facultyRepository
+    EventsPublisher eventsPublisher
+    FacultyManagementService service
+
+    def setup() {
+        facultyRepository = new FacultySimpleRepository()
+        eventsPublisher = new SimpleEventsPublisher()
+        service = new DomainFacultyManagementService(facultyRepository, eventsPublisher)
+    }
+
+    def "after successful employment resignation notification should be sent"() {
+        given: "faculty with hired professor"
+        def newFaculty = newFaculty()
+        def hired = newProfessor(newFaculty.id())
+        def snapshot = newFaculty.toBuilder()
+                .professor(hired)
+                .build()
+        facultyRepository.save(snapshot)
+
+        when: "professor resigns from employment"
+        def employmentResignationResult = service.resignFromEmployment(hired)
+
+        then: "resignation should be received successfully"
+        employmentResignationResult.value().isPresent()
+
+        and: "faculty should be updated in repository"
+        def maybeFaculty = facultyRepository.findById(snapshot.id())
+        maybeFaculty.isPresent()
+        def faculty = maybeFaculty.get()
+        def maybeResigned = faculty.professors().stream()
+                .filter { it.id() == hired.id() }
+                .findFirst()
+        maybeResigned.isPresent()
+        def resigned = maybeResigned.get()
+
+        and: "a specific notification should be sent"
+        eventsPublisher.events().contains(new ProfessorHiringResignationEvent(resigned))
+    }
+
+    def "after unsuccessful employment resignation notification should not be sent"() {
+        given: "professor hired at not known faculty"
+        def notKnownFacultyId = new FacultyId(UUID.randomUUID())
+        def notKnownProfessor = newProfessor(notKnownFacultyId)
+
+        when: "professor resigns from employment"
+        def employmentResignationResult = service.resignFromEmployment(notKnownProfessor)
+
+        then: "resignation should not be received successfully"
+        employmentResignationResult.value().isEmpty()
+        employmentResignationResult.resultReason() == EmploymentResignationResultReason.INCORRECT_FACULTY_ID
+
+        and: "should not be stored in any repository"
+        !facultyRepository.existsById(notKnownFacultyId)
+
+        and: "no notification should be sent"
+        eventsPublisher.events().isEmpty()
+    }
+
+    def "after successful enrollment resignation notification should be sent"() {
+        given: "faculty with enrolled student"
+        def newFaculty = newFaculty()
+        def enrolled = newStudent(newFaculty.id())
+        def faculty = newFaculty.toBuilder()
+                .student(enrolled)
+                .build()
+        facultyRepository.save(faculty)
+
+        when: "student resigns from enrollment"
+        def enrollmentResignationResult = service.resignFromEnrollment(enrolled)
+
+        then: "resignation should be received successfully"
+        enrollmentResignationResult.value().isPresent()
+
+        and: "faculty should be updated in repository"
+        def maybeFaculty = facultyRepository.findById(faculty.id())
+        maybeFaculty.isPresent()
+        def course = maybeFaculty.get()
+        def maybeResigned = course.students().stream()
+                .filter { it.id() == enrolled.id() }
+                .findFirst()
+        maybeResigned.isPresent()
+        def resigned = maybeResigned.get()
+
+        and: "a specific notification should be sent"
+        eventsPublisher.events().contains(new StudentEnrollmentResignationEvent(resigned))
+    }
+
+    def "after unsuccessful enrollment resignation notification should not be sent"() {
+        given: "student enrolled at not known faculty"
+        def notKnownFacultyId = new FacultyId(UUID.randomUUID())
+        def notKnownStudent = newStudent(notKnownFacultyId)
+
+        when: "student resigns from enrollment"
+        def enrollmentResignationResult = service.resignFromEnrollment(notKnownStudent)
+
+        then: "resignation should not be received successfully"
+        enrollmentResignationResult.value().isEmpty()
+        enrollmentResignationResult.resultReason() == EnrollmentResignationResultReason.INCORRECT_FACULTY_ID
+
+        and: "should not be stored in any repository"
+        !facultyRepository.existsById(notKnownFacultyId)
+
+        and: "no notification should be sent"
+        eventsPublisher.events().isEmpty()
+    }
+}
