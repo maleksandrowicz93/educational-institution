0338252cd93642a042b0e74e7fb0b4cc702d4a76
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-18 15:32:16 +0200

    add result reasons to human resources domain

---
+++ human-resources/human-resources-domain/src/main/java/com/github/maleksandrowicz93/educational/institution/result/reasons/ProfessorEmploymentFailureReason.java
@@ -0,0 +1,21 @@
+package com.github.maleksandrowicz93.educational.institution.result.reasons;
+
+import com.github.maleksandrowicz93.educational.institution.common.ResultReason;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(makeFinal = true)
+@RequiredArgsConstructor
+public enum ProfessorEmploymentFailureReason implements ResultReason {
+
+    FIELDS_OF_STUDY_NOT_MATCHED("Field of study not matched."),
+    TOO_LITTLE_EXPERIENCE("Too little experience."),
+    NO_VACANCY("No vacancy at faculty.");
+
+    String text;
+
+    @Override
+    public String text() {
+        return text;
+    }
+}
