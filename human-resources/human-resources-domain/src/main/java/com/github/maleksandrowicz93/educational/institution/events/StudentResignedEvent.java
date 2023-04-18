5ad45801fe62c77b46d957227eb1d3a7c9ad2eb0
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-18 10:55:13 +0200

    create human resources domain events

---
+++ human-resources/human-resources-domain/src/main/java/com/github/maleksandrowicz93/educational/institution/events/StudentResignedEvent.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.educational.institution.events;
+
+import com.github.maleksandrowicz93.educational.institution.vo.FacultyId;
+import com.github.maleksandrowicz93.educational.institution.vo.ProfessorId;
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+public record StudentResignedEvent(
+        @NonNull ProfessorId professorId,
+        @NonNull FacultyId facultyId
+) implements ResignationEvent {
+}
