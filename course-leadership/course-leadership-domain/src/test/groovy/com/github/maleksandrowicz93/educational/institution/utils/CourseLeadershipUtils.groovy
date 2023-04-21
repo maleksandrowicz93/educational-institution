bc97a7f982667240e8445b6ef53671e3e4f25dfa
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2023-04-21 13:37:48 +0200

    fix CourseLeadershipUtils

--- course-leadership/course-leadership-domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/utils/CourseLeadershipUtils.groovy
+++ course-leadership/course-leadership-domain/src/test/groovy/com/github/maleksandrowicz93/educational/institution/utils/CourseLeadershipUtils.groovy
@@ -1,9 +1,11 @@
 package com.github.maleksandrowicz93.educational.institution.utils
 
+import com.github.maleksandrowicz93.educational.institution.enums.LeadershipState
 import com.github.maleksandrowicz93.educational.institution.vo.CourseId
 import com.github.maleksandrowicz93.educational.institution.vo.CourseLeadershipSnapshot
 import com.github.maleksandrowicz93.educational.institution.vo.ProfessorId
 
+import static com.github.maleksandrowicz93.educational.institution.enums.LeadershipState.FREE
 import static com.github.maleksandrowicz93.educational.institution.enums.LeadershipState.LED
 import static com.github.maleksandrowicz93.educational.institution.utils.FieldOfStudyUtils.FIELDS_OF_STUDY
 import static com.github.maleksandrowicz93.educational.institution.utils.ThresholdsUtils.courseOvertakingThresholds
@@ -13,20 +15,20 @@ class CourseLeadershipUtils {
     private CourseLeadershipUtils() {}
 
     static def ledCourse(ProfessorId professorId) {
-        courseConfiguredWith(professorId)
+        courseConfiguredWith(professorId, LED)
     }
 
-    private static def courseConfiguredWith(ProfessorId professorId) {
+    private static def courseConfiguredWith(ProfessorId professorId, LeadershipState leadershipState) {
         CourseLeadershipSnapshot.builder()
                 .id(new CourseId(UUID.randomUUID()))
                 .thresholds(courseOvertakingThresholds())
                 .fieldsOfStudy(FIELDS_OF_STUDY)
                 .leadingProfessor(professorId)
-                .leadershipState(LED)
+                .leadershipState(leadershipState)
                 .build()
     }
 
     static def freeCourse() {
-        courseConfiguredWith(null)
+        courseConfiguredWith(null, FREE)
     }
 }
